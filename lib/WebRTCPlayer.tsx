bb7e12ee28160bc5a7ed974e1e4cc0917d030297
Preston <pbarnett@uas-gs.com>
2021-06-10 11:38:59 -0500

    asdf

--- lib/WebRTCPlayer.tsx
+++ lib/WebRTCPlayer.tsx
@@ -184,7 +184,7 @@ export class WebRTCPlayer extends React.Component<Props, State> implements IPlay
     if (!streamName) {
       throw new Error('Stream Name is required.')
     }
-    while (hash.hashed == undefined) {
+    while (hash == undefined) {
     (function(i) {
       setTimeout(function() {
         console.log("timing out")
